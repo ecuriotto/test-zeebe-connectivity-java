8228e72b15fb5ad7a77fff51bdbfc7e5a12e6fc8
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2024-09-10 15:12:21 +0200

    list brokers details

--- src/main/java/com/camunda/academy/PaymentApplication.java
+++ src/main/java/com/camunda/academy/PaymentApplication.java
@@ -1,12 +1,44 @@
 package com.camunda.academy;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PaymentApplication {
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.BrokerInfo;
+import io.camunda.zeebe.client.api.response.PartitionInfo;
 
-	public static void main(final String... args) {
-		SpringApplication.run(PaymentApplication.class, args);
-	}
+import java.util.List;
+
+@SpringBootApplication
+public class PaymentApplication implements CommandLineRunner {
+
+    @Autowired
+    private ZeebeClient client;
+
+    public static void main(final String... args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Retrieve brokers information
+        List<BrokerInfo> brokers = client.newTopologyRequest()
+            .send()
+            .join()
+            .getBrokers();
+        
+        // Iterate through brokers and print information
+        for (BrokerInfo broker : brokers) {
+            System.out.println("Broker ID: " + broker.getNodeId());
+            System.out.println("Broker Address: " + broker.getAddress());
+
+            List<PartitionInfo> partitions = broker.getPartitions();
+            for (PartitionInfo partition : partitions) {
+                System.out.println("Partition ID: " + partition.getPartitionId());
+                System.out.println("Is Leader: " + partition.isLeader());
+            }
+        }
+    }
 }
